0252ef30b3cfe8d4aca56f6f6931242822164588
Kelly Heller <kkheller@cedrus.com>
2015-05-15 11:20:24 -0700

    committing SOME of the results of using prettifier 'style cop' tool yapf-0.1.8 (more cleanup to follow, but have to look into a bug in yapf-0.1.8 first)

--- finick/finicky/basics.py
+++ finick/finicky/basics.py
@@ -1,6 +1,5 @@
-
 from __future__ import print_function
-from __future__ import division # py3 style. division promotes to floating point.
+from __future__ import division  # py3 style. division promotes to floating point.
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
@@ -15,7 +14,8 @@ import locale
 # Python-3.4.3//Tools/i18n/pygettext.py finick/finicky/*py *py finick/*py # to generate pot
 # The hard-coded path is lame. This is a version 0.0 kind of thing.
 # make sure this is called as finick/start.py (use os.sep). this way we can count on CWD for finding locale folder
-_t = gettext.translation('finick_code_review', './finick/locale', ['en_US']) #, fallback=True)
+_t = gettext.translation('finick_code_review', './finick/locale',
+                         ['en_US'])  #, fallback=True)
 try:
     # for older python 2:
     _ = _t.ugettext
@@ -27,13 +27,15 @@ http://www.wefearchange.org/2012/06/the-right-way-to-internationalize-your.html
 """
 
 
-def prelaunch_checklist_open( calling_filename ):
-    return _prelaunch_checklist( calling_filename, True )
+def prelaunch_checklist_open(calling_filename):
+    return _prelaunch_checklist(calling_filename, True)
 
-def prelaunch_checklist_close( calling_filename ):
-    return _prelaunch_checklist( calling_filename, False )
 
-def _prelaunch_checklist( calling_filename, is_session_starting ):
+def prelaunch_checklist_close(calling_filename):
+    return _prelaunch_checklist(calling_filename, False)
+
+
+def _prelaunch_checklist(calling_filename, is_session_starting):
 
     # make sure this is called as finick/start.py (use os.sep). this way we can count on CWD for finding config
     # config integrity check. (older than start of session to prevent mid-session reconfig).
@@ -44,7 +46,7 @@ def _prelaunch_checklist( calling_filename, is_session_starting ):
 
     if len(all_cwd_inis) != 1:
         # eventually we will allow the INI file to be passed as an argument
-        raise FinickError("you must have exactly one (and only one) ini file in the CWD")
+        raise FinickError(
+            "you must have exactly one (and only one) ini file in the CWD")
 
-    return finicky.parse_config.FinickConfig( all_cwd_inis[0] )
-
+    return finicky.parse_config.FinickConfig(all_cwd_inis[0])
--- finick/finicky/db_file.py
+++ finick/finicky/db_file.py
@@ -1,6 +1,5 @@
-
 from __future__ import print_function
-from __future__ import division # py3 style. division promotes to floating point.
+from __future__ import division  # py3 style. division promotes to floating point.
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
@@ -11,14 +10,17 @@ from finicky.basics import _
 import os
 from io import open
 
-def AssertType_DbTextFile( o ):
+
+def AssertType_DbTextFile(o):
     rhs = DbTextFile.dummyinstance()
     incoming_type = str(type(o))
     if type(o) != type(rhs):
-        raise FinickError("AssertType_DbTextFile failed. DbTextFile was required, but instead we got: " + incoming_type)
+        raise FinickError(
+            "AssertType_DbTextFile failed. DbTextFile was required, but instead we got: "
+            + incoming_type)
+
 
 class DbTextFile(object):
-
     @classmethod
     def create_from_file(cls, finick_config):
         return cls(False, finick_config)
@@ -27,7 +29,7 @@ class DbTextFile(object):
     def dummyinstance(cls):
         return cls(True)
 
-    def __init__(self, is_dummy, finick_config=None ):
+    def __init__(self, is_dummy, finick_config=None):
 
         # if some on a team need to keep older file format, this will need to be configurable:
         self.__CURR_FILE_VER = 0
@@ -37,46 +39,47 @@ class DbTextFile(object):
         self.__version_from_fileread = -1
 
         if False == is_dummy:
-            self._initialize_from_file( finick_config )
+            self._initialize_from_file(finick_config)
             pass
 
     def _fail_setter(self, value):
-        raise FinickError("The value you are trying to assign to in DbTextFile is read-only.")
+        raise FinickError(
+            "The value you are trying to assign to in DbTextFile is read-only.")
 
     is_ok      = property(lambda s : s.__is_ok,         _fail_setter)
 
-    def _initialize_from_file( self, finick_config ):
+    def _initialize_from_file(self, finick_config):
 
-        finicky.parse_config.AssertType_FinickConfig( finick_config )
+        finicky.parse_config.AssertType_FinickConfig(finick_config)
 
         # before we check the content of the file, we should check whether it is known (committed) to git
 
         expected_db = finick_config.confdir + os.sep + finick_config.configname + '.txt'
 
-        db_found = os.path.isfile( expected_db )
+        db_found = os.path.isfile(expected_db)
         db_committed = False
         is_ok = False
 
         if not db_found:
-            print (expected_db, 'not found')
+            print(expected_db, 'not found')
         else:
-            db_committed = finicky.gitting.git_repo_contains_committed_file( finick_config, os.path.abspath(expected_db) )
+            db_committed = finicky.gitting.git_repo_contains_committed_file(
+                finick_config, os.path.abspath(expected_db))
 
         if not db_committed:
-            print (expected_db, 'not committed')
+            print(expected_db, 'not committed')
 
         if db_found and db_committed:
             try:
-                with open( expected_db, encoding='utf-8' ) as f:
+                with open(expected_db, encoding='utf-8') as f:
                     for line in f:
-                        print ( _('someline') )
+                        print(_('someline'))
 
             except:
                 configfile = finick_config.confdir + os.sep + finick_config.configname
                 err_msg = 'Missing or unreadable DB file. (Create an empty file for first use.) '
-                err_msg += 'While using config \''+ configfile +'\', you must have the DB file \''+ expected_db +'\''
-                raise FinickError( err_msg )
-
+                err_msg += 'While using config \'' + configfile + '\', you must have the DB file \'' + expected_db + '\''
+                raise FinickError(err_msg)
 
         self.__is_ok = is_ok
         # since all went well, store the config for use by other member functions later:
@@ -108,19 +111,21 @@ class DbTextFile(object):
         pass
 
 
-def db_integrity_check_open( finick_config ):
+def db_integrity_check_open(finick_config):
 
-    return _db_integrity_check( finick_config, True )
+    return _db_integrity_check(finick_config, True)
 
-def db_integrity_check_close( finick_config ):
+
+def db_integrity_check_close(finick_config):
 
     # reviews file should now be properly upgraded already.
     # last line in DB file should be file version info
-    return _db_integrity_check( finick_config, False )
+    return _db_integrity_check(finick_config, False)
 
-def _db_integrity_check( finick_config, is_session_starting ):
 
-    db_handle = DbTextFile.create_from_file( finick_config )
+def _db_integrity_check(finick_config, is_session_starting):
+
+    db_handle = DbTextFile.create_from_file(finick_config)
 
     if db_handle.is_ok:
         return db_handle
@@ -128,13 +133,12 @@ def _db_integrity_check( finick_config, is_session_starting ):
         return None
 
 
+def db_open_session(finick_config, db_handle):
 
-def db_open_session( finick_config, db_handle ):
+    finicky.parse_config.AssertType_FinickConfig(finick_config)
+    AssertType_DbTextFile(db_handle)
 
-    finicky.parse_config.AssertType_FinickConfig( finick_config )
-    AssertType_DbTextFile( db_handle )
-
-    print ('db open session')
+    print('db open session')
 
     # if we are starting one session, there must not be any other session in progress (even from other INI file) [job of git_establish_session_readiness]
 
@@ -142,11 +146,8 @@ def db_open_session( finick_config, db_handle ):
     db_handle.add_new_commits()
     db_handle.flush_back_to_disk()
 
-    # generate assignments for this session.
     assignments = db_handle.generate_assignments_for_this_session()
 
-    # emit a reminder message about todo items
     todos = db_handle.generate_todos_for_this_session()
 
     return assignments, todos
-
