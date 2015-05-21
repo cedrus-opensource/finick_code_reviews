35a311640bbfb892696910e49df456d6cd010b87
Kelly Heller <kkheller@cedrus.com>
2015-05-21 08:56:51 -0700

    just reordering 2 lines so that the 'commithash' line is directly under a line where i can see if the '_fail_setter)' string remains column-aligned with all above columns. yapf used to erroneously edit the code to dis-align the columnar alignment, but now github.com/google/yapf/issues/146 has been repaired

--- finick/finicky/db_row.py
+++ finick/finicky/db_row.py
@@ -100,9 +100,9 @@ class DbRow(object):
 
     committer  = property(lambda s : s.__committer,  _fail_setter)
 
-    comment    = property(lambda s : s.__action_comment, _fail_setter)
+    commithash = property(lambda s : s.__commit_hash,_fail_setter)
 
-    commithash = property(lambda s : s.__commit_hash, _fail_setter)
+    comment    = property(lambda s : s.__action_comment, _fail_setter)
 
     # yapf: enable
 
