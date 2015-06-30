9fca7d198749e68253c47987ce5bd8926451c29c
Kelly Heller <kkheller@cedrus.com>
2015-06-29 17:40:15 -0700

    from now on, in addition to the per-repo 'reviews.ini' config file, you also need to provide a 'finickmail.ini' file with smtp information, so that finick can send email on your behalf. The mail-related ini file does *not* get checked in under git version-control in your repo-under-review. The 'reviews.ini' config file should be kept in version control, but your user-specific 'finickmail.ini' file should *not*.

--- finick/finicky/parse_config.py
+++ finick/finicky/parse_config.py
@@ -44,12 +44,19 @@ class FinickConfig(object):
         self.__invoker_eml = ''
         self.__only_maint = False
         self.__requests = []
+        self.__mailserver = ''
+        self.__mailport = 0
+        self.__maillogin = ''
+        self.__mailpword = ''
 
         if not parsed_args is None:
             self._process_args(parsed_args)
 
         if False == defaults_only:
             self._initialize_from_file(file_location)
+            self._read_userfile_settings()
+            # todo: we need to sanity check this more thoroughly before setting to true
+            self.__is_ok = True
 
     @classmethod
     def dummyinstance(cls):
@@ -107,6 +114,14 @@ class FinickConfig(object):
 
     verbosity  = property(lambda s : s.__verbose,       _fail_setter)
 
+    mailserver = property(lambda s : s.__mailserver,    _fail_setter)
+
+    mailport   = property(lambda s : s.__mailport,      _fail_setter)
+
+    maillogin  = property(lambda s : s.__maillogin,     _fail_setter)
+
+    mailpword  = property(lambda s : s.__mailpword,     _fail_setter)
+
     # email address of the reviewer (the person driving the review session)
     reviewer   = property(lambda s : s.__invoker_eml,   _set_email  )
 
@@ -156,8 +171,50 @@ class FinickConfig(object):
         self.__cfgdir = os.path.normpath(location)
         self.__verbose = int(cf.get('GitReviews', 'Verbosity'))
 
-        # todo: we need to sanity check this more thoroughly before setting to true
-        self.__is_ok = True
+    def _read_userfile_settings(self):
+
+        appdata_or_empty = os.getenv('APPDATA', '')
+
+        appdata_ini_loc = os.path.normpath(
+            appdata_or_empty + os.sep + 'finickmail.ini')
+        # on unix-like platforms, use the leading dot (.) to make a hidden file:
+        tilde_ini_loc = os.path.normpath(
+            os.path.expanduser('~') + os.sep + '.finickmail.ini')
+
+        required_loc = '\'' + tilde_ini_loc + '\''
+
+        if appdata_or_empty != '':
+            required_loc = 'either \'' + appdata_ini_loc + '\' or else ' + required_loc
+
+        appdata_item_found = os.path.isfile(appdata_ini_loc)
+        tilde_item_found = os.path.isfile(tilde_ini_loc)
+
+        cf = configparser.ConfigParser()
+
+        if not (appdata_item_found or tilde_item_found):
+            raise FinickError(
+                'User-data ini file missing. We require this ' +
+                'secondary ini file (which must reside OUTSIDE ' +
+                'the directory of the git-repo under review) ' +
+                'for email smtp settings. Put the file here, please: ' +
+                required_loc)
+
+        elif appdata_item_found:
+            # it is crucial to open with utf-8 for py2/py3 cross-compatibility
+            cf.readfp(open(appdata_ini_loc, encoding='utf-8'))
+
+        elif tilde_item_found:
+            # it is crucial to open with utf-8 for py2/py3 cross-compatibility
+            cf.readfp(open(tilde_ini_loc, encoding='utf-8'))
+
+        else:
+            print(
+                'We should NEVER execute this line. Did our boolean logic in this if/elif/elif get messed up?')
+
+        self.__mailserver = cf.get('FinickMail', 'MailServerString')
+        self.__mailport = int(cf.get('FinickMail', 'MailPortInteger'))
+        self.__maillogin = cf.get('FinickMail', 'MailAccountLoginName')
+        self.__mailpword = cf.get('FinickMail', 'MailAccountPword')
 
     def _process_args(self, parsed_args):
 
